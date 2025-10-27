db1ec22e0c011f076964c321785c5f34d6b326c8
karlwsmad721 <karlwsmad721@gmail.com>
2025-10-27 14:16:54 +0300

    fix: use gunicorn and 0.0.0.0

--- app.py
+++ app.py
@@ -616,6 +616,5 @@ def init_db():
         db.session.commit()
         print("Database initialized!")
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
